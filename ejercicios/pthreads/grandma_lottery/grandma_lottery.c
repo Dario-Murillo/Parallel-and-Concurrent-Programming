ff9054902aa5b707051dc1c42f6dd0c41e288bc6
Dario Murillo Chaverri <c15406@ECCI106L385806.ecci.ucr.ac.cr>
2023-03-21 17:48:40 -0600

    se agrega granda_lottery

---
+++ ejercicios/pthreads/grandma_lottery/grandma_lottery.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/random.h>
+#include <stdio.h>
+
+void* number(void* data);
+
+
+int main(void) {
+
+
+    
+    void* numero_ran1 = NULL;
+    size_t numero_ran2 = 0;
+
+    // void* temp;
+    pthread_t thread_ran1;
+    pthread_create(&thread_ran1, NULL, number, NULL);
+
+    pthread_t thread_ran2;
+    pthread_create(&thread_ran2, NULL, number, NULL);
+
+
+    pthread_join(thread_ran1, &numero_ran1);
+    pthread_join(thread_ran2, (void*)numero_ran2);
+
+    //numero_ran1 = (size_t) temp;
+    printf("Numero:%zu\n", (size_t)numero_ran1);
+    
+
+}
+
+
+void* number(void* data) {
+  
+  (void) data;
+  size_t numeroran =  99; 
+  void* ptr = (void*)numeroran;
+  return ptr;
+}
