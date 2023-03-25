2a2dd75a7384a49a5273ee616c07d89067d19dc8
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-03-25 00:27:27 -0600

    grandma_lottery-terminado

--- ejercicios/pthreads/grandma_lottery/src/grandma_lottery.c
+++ ejercicios/pthreads/grandma_lottery/src/grandma_lottery.c
@@ -1,9 +1,10 @@
+// Copyright 2023 <Dario Murillo Chaverri C15406>
 #include <pthread.h>
 #include <stdlib.h>
 #include <sys/random.h>
 #include <stdio.h>
 
-// declaracion de la subrutina number 
+// declaracion de la subrutina number
 void* number(void* data);
 
 /**
@@ -11,22 +12,24 @@ void* number(void* data);
 */
 int main(void) {
     size_t numero_ran1 = 0;
-    unsigned int seed1 =  time(NULL); // generamos una 'seed' 
+    unsigned int seed1 =  time(NULL);  // generamos una 'seed'
     pthread_t* threads = (pthread_t*) malloc(2 * sizeof(pthread_t));
     for (int thread_num = 0; thread_num < 2; thread_num++) {
-      pthread_create(&threads[thread_num], NULL, number, (void*)&seed1);
+      pthread_create(&threads[thread_num], NULL, number, (void*) &seed1);
     }
     for (int thread_num = 0; thread_num < 2; thread_num++) {
-      pthread_join(threads[thread_num], (void*) &numero_ran1);
+      pthread_join(threads[thread_num],  (void*) &numero_ran1);
       printf("Numero del hilo %d: %zu\n" , thread_num, numero_ran1);
     }
-    free(threads); // liberar la memoria dinamica solicitada para crear los hilos
-    // free(dirr);
-} //  end main
+    free(threads);  // liberar la memoria dinamica
+}  //  end main
+
 
 /**
- * @brief subrutina number 
- * @param data recibe un seed unico que es usado para el metodo rand_r
+ * @brief subrutina encargada de devolver el numero generar
+ * numero pseudoaleatorio y retornarlo como una falsa direccion
+ * de memoria
+ * @param data
 */
 void* number(void* data) {
   unsigned int *seed = data;  // recibir la seed unica
@@ -34,17 +37,4 @@ void* number(void* data) {
   size_t cast = (size_t) number;
   void* retorno = (void*) cast;
   return retorno;
-} // end number
-
-//variante que devuelve la dirrecion de memoria del numero generado
-//funciona de igual manera y no da errores 
-/**
- * void* number(void* data) {
- * unsigned int *seed = data; // recibir la seed unica
- * int number = rand_r(seed) % 99; // evitar que el numero sea mayor a 99
-   int* dirr = (int*) malloc(number * size_of(int))
- * return dirr;
- * } // end number
- * 
- * 
-*/
+}  // end number
