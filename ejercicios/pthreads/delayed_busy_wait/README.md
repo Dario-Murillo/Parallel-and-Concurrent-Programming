513379526ee28413bf676660a139d00f2c589ec0
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-03-27 23:19:24 -0600

    se agrega ejercicio12-delayed_busy_wait

---
+++ ejercicios/pthreads/delayed_busy_wait/README.md
@@ -0,0 +1,63 @@
+# Enunciado del ejercicio
+Espera activa con retraso. ¿Se corrige el problema de la espera activa si en lugar de tener un ciclo vacío, se espera un cierto tiempo? Copie su carpeta ejemplos/pthreads/hello_order_busywait a ejercicios/pthreads/delayed_busy_wait. Permita que el usuario pueda invocar su programa dos argumentos de línea de comandos: la cantidad de hilos a crear, y la cantidad de microsegundos a esperar cuando no es el turno del hilo de ejecución.
+
+##Espera activa con retraso constante. 
+Si no es el turno de un hilo, éste espera una cantidad constante de microsegundos, algo como:
+
+// Constant delayed busy waiting: wait until it is my turn
+while (next_thread < my_thread_id) {
+  usleep(delay);
+}
+
+Recuerde probar la calidad de su código (sanitizers, linter). Luego ejecute al menos tres veces su solución (sin sanitizers) con la cantidad máxima de hilos de ejecución que su sistema operativo le permite crear y una espera de 50µs. Escriba en el readme.md del ejercicio la mayor de las duraciones que obtuvo de sus corridas. ¿Hubo una mejora de la espera constante respecto a la espera activa pura?
+
+Al correr el programa en la version de espera activa con retraso constante 
+Primera Duracion: 5.701099971s
+Segunda Duracion: 5.696040950s
+Tercera Duracion: 7.858201478s
+
+Si hubo una mejora comparado con la espera activa pura
+
+
+##Espera activa con retraso pseudoaleatorio. 
+Altere su solución al ejercicio para que en lugar de esperar exactamente la cantidad de microsegundos indicada por el usuario, espere por una cantidad pseudoaleatoria de microsegundos cuyo límite es el número indicado por el usuario en el segundo argumento de línea de comandos. Sugerencia: puede usar compilación condicional para implementar esta variación. La espera varía en cada iteración del ciclo de espera activa, algo como:
+
+// Random delayed busy waiting: wait until it is my turn
+while (next_thread < my_thread_id) {
+	const unsigned my_delay = rand_r(&my_seed) % max_delay;
+  usleep(my_delay);
+}
+
+Ejecute al menos tres veces su solución pseudoaleatoria con la cantidad máxima de hilos y un máximo de espera de 50µs. Tome la mayor de las duraciones. ¿Hubo una mejora de la espera pseudoaleatoria respecto a la espera constante?
+
+Al correr el programa en la version de espera activa con retraso pseudoaleatorio
+Primera Duracion: 3.907234565s
+Segunda Duracion: 4.230741799s
+Tercera Duracion: 4.545225735s
+
+Si hubo una mejora comparado con la espera activa con retraso constante
+
+
+
+##Comparación de las esperas. 
+¿Mejora el tiempo de ejecución de los dos tipos de esperas (constante y pseudoaleatoria) si disminuye o incrementa la espera máxima de los hilos? Haga al menos un par de ejecuciones con al menos los siguientes tiempos de espera:
+
+1µs
+constante = 3.801403693s
+
+5µs
+constante = 4.220764891s
+
+25µs
+constante = 4.384902108s
+
+50µs
+constante = 5.903071652s
+
+75µs
+constante = 6.774459693s
+
+100µs
+constante = 7.518732852s
+
+
---
+++ ejercicios/pthreads/delayed_busy_wait/src/delayed_busy_wait.c
@@ -0,0 +1,158 @@
+// Copyright 2021 Jeisson Hidalgo <jeisson.hidalgo@ucr.ac.cr> CC-BY 4.0
+#define _POSIX_C_SOURCE 199309L   // para CLOCK_MONOTONIC
+#define _DEFAULT_SOURCE
+#include <assert.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+// thread_shared_data_t
+// estructura de datos de memoria compartida
+// en este caso el dato que comparten todos los hilos es el totaol de hilos
+typedef struct shared_data {
+  uint64_t next_thread;  // variable compartida que identifica el prox hilo
+  uint64_t delay;
+  uint64_t thread_count;
+} shared_data_t;
+
+// thread_private_data_t
+// estructura de datos de memoria privada
+// en este caso el dato privado de cada hilo, es su respectivo numero
+typedef struct private_data {
+  uint64_t thread_number;  // rank
+  shared_data_t* shared_data;
+} private_data_t;
+
+/**
+ * @brief ...
+ */
+void* greet(void* data);
+int create_threads(shared_data_t* shared_data);
+
+// procedure main(argc, argv[])
+int main(int argc, char* argv[]) {
+  int error = EXIT_SUCCESS;
+  // create thread_count as result of converting argv[1] to integer
+  // thread_count := integer(argv[1])
+  uint64_t thread_count = sysconf(_SC_NPROCESSORS_ONLN);
+  uint64_t delay;
+  if (argc == 3) {
+    if (sscanf(argv[1], "%" SCNu64, &thread_count) == 1 &&
+        sscanf(argv[2], "%" SCNd64 , &delay) == 1) {
+    } else {
+      fprintf(stderr, "Error: invalid thread count\n");
+      return 11;
+    }
+  }
+
+  shared_data_t* shared_data = (shared_data_t*)calloc(1, sizeof(shared_data_t));
+  if (shared_data) {
+    shared_data->next_thread = 0;  // inicializar el conteo de hilos en 0
+    shared_data->thread_count = thread_count;
+    shared_data->delay = delay;
+    // estructura de datos solicitada para medir el tiempo de ejecucion
+    struct timespec start_time, finish_time;
+
+    clock_gettime(CLOCK_MONOTONIC, &start_time);  // el reloj toma el tiempo
+
+    error = create_threads(shared_data);  // los hilos trabajan
+
+    clock_gettime(CLOCK_MONOTONIC, &finish_time);  // el reloj toma el tiempo
+
+    // determinamos cuanto duro en hacer el trabajo
+    double elapsed_time = finish_time.tv_sec - start_time.tv_sec +
+      (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;
+    
+
+    printf("Execution time: %.9lfs\n", elapsed_time);
+    printf("SOY EL DELAY:%zu", shared_data->delay);
+    free(shared_data);  // liberacion de la memoria
+  } else {
+    fprintf(stderr, "Error: could not allocate shared data\n");
+    return 12;
+  }
+  return error;
+}  // end procedure
+
+/**
+ * @brief funciona como el hilo principal es el encargado de multiples acciones
+ * desde crear hilos, haciendo uso de memoria dinamica, como una impresion, 
+ * y luego la liberacion de la memoria usada
+ * @param shared_data recibe del main cuantos hilos se van a crear segun
+ * lo indica por el usuario, en caso de no recibir nada usa el numero
+ * de nucleos
+*/
+int create_threads(shared_data_t* shared_data) {
+  int error = EXIT_SUCCESS;
+  // for thread_number := 0 to thread_count do
+  pthread_t* threads = (pthread_t*)
+    malloc(shared_data->thread_count * sizeof(pthread_t));
+  private_data_t* private_data = (private_data_t*)
+    calloc(shared_data->thread_count, sizeof(private_data_t));
+  if (threads && private_data) {
+    for (uint64_t thread_number = 0; thread_number < shared_data->thread_count
+        ; ++thread_number) {
+      private_data[thread_number].thread_number = thread_number;
+      private_data[thread_number].shared_data = shared_data;
+      // create_thread(greet, thread_number)
+      error = pthread_create(&threads[thread_number], /*attr*/ NULL, greet
+        , /*arg*/ &private_data[thread_number]);
+      if (error == EXIT_SUCCESS) {
+      } else {
+        fprintf(stderr, "Error: could not create secondary thread\n");
+        error = 21;
+        break;
+      }
+    }
+
+    // print "Hello from main thread"
+    printf("Hello from main thread\n");
+
+    for (uint64_t thread_number = 0; thread_number < shared_data->thread_count
+        ; ++thread_number) {
+      pthread_join(threads[thread_number], /*value_ptr*/ NULL);
+    }
+
+    free(private_data);
+    free(threads);
+  } else {
+    fprintf(stderr, "Error: could not allocate %" PRIu64 " threads\n"
+      , shared_data->thread_count);
+    error = 22;
+  }
+
+  return error;
+}
+
+/**
+  * @brief metodo que usado por los hilos secundarios indican:
+  * numero de hilos y total de hilos
+  * pero esta vez los hilos van en orden
+  * @param data
+  * 
+*/
+void* greet(void* data) {
+  assert(data);  // evita que nos envien datos nulos
+  private_data_t* private_data = (private_data_t*) data;
+  shared_data_t* shared_data = private_data->shared_data;
+
+  // espero en un ciclo infinito hasta que sea mi turno
+  while (shared_data->next_thread < private_data->thread_number) {
+    usleep(shared_data->delay);
+  }  // end while
+  // nunca hacer este while es una terrible practica
+
+  // print "Hello from secondary thread"
+  printf("Hello from secondary thread %" PRIu64 " of %" PRIu64 "\n"
+    , private_data->thread_number, shared_data->thread_count);
+
+  // indica al siguiente hilo que puede seguir
+  ++shared_data->next_thread;
+
+  return NULL;
+}  // end procedure
+
