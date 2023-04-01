d6d67149353fab772b9333f0f09d6c7d4c9b226a
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-04-01 13:47:51 -0600

    se agrega documentacion al ejemplo

--- ejemplos/pthreads/hello_order_semaphor/src/hello_order_semaphor.c
+++ ejemplos/pthreads/hello_order_semaphor/src/hello_order_semaphor.c
@@ -1,5 +1,5 @@
 // Copyright 2021 Jeisson Hidalgo <jeisson.hidalgo@ucr.ac.cr> CC-BY 4.0
-
+#define _POSIX_C_SOURCE 199309L
 #include <assert.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -11,12 +11,16 @@
 #include <unistd.h>
 
 // thread_shared_data_t
+// estructura de datos de memoria compartida
+// en este caso el dato que comparten todos los hilos es el totaol de hilos
 typedef struct shared_data {
-  sem_t* can_greet;
+  sem_t* can_greet;  // variable tipo sem_t que almacena los semaforos
   uint64_t thread_count;
 } shared_data_t;
 
 // thread_private_data_t
+// estructura de datos de memoria privada
+// en este caso el dato privado de cada hilo, es su respectivo numero
 typedef struct private_data {
   uint64_t thread_number;  // rank
   shared_data_t* shared_data;
@@ -44,14 +48,19 @@ int main(int argc, char* argv[]) {
 
   shared_data_t* shared_data = (shared_data_t*)calloc(1, sizeof(shared_data_t));
   if (shared_data) {
+    // creamos un arreglo de semaforos
     shared_data->can_greet = (sem_t*) calloc(thread_count, sizeof(sem_t));
     shared_data->thread_count = thread_count;
 
+    // se crean los semaforos necesarios para cada hilo
     for (uint64_t thread_number = 0; thread_number < shared_data->thread_count
         ; ++thread_number) {
       // can_greet[thread_number] := create_semaphore(not thread_number)
       error = sem_init(&shared_data->can_greet[thread_number], /*pshared*/ 0
         , /*value*/ !thread_number);
+      // el pshared es 0 para no compartir entre procesos
+      // se inician todos los valores en !thread_number, es decir negamos
+      // el numero de hilo
     }
 
     if (shared_data->can_greet) {
@@ -79,7 +88,15 @@ int main(int argc, char* argv[]) {
   return error;
 }  // end procedure
 
-
+/**
+ * @brief funciona como el hilo principal es el encargado de multiples acciones
+ * desde crear hilos, haciendo uso de memoria dinamica, como una impresion, 
+ * y luego la liberacion de la memoria usada, tambien libera la memoria
+ * de los semaforos
+ * @param shared_data recibe del main cuantos hilos se van a crear segun
+ * lo indica por el usuario, en caso de no recibir nada usa el numero
+ * de nucleos
+*/
 int create_threads(shared_data_t* shared_data) {
   int error = EXIT_SUCCESS;
   // for thread_number := 0 to thread_count do
@@ -117,6 +134,7 @@ int create_threads(shared_data_t* shared_data) {
         ; ++thread_number) {
       pthread_join(threads[thread_number], /*value_ptr*/ NULL);
       sem_destroy(&shared_data->can_greet[thread_number]);
+      // liberacion de memoria de los semaforos
     }
 
     free(private_data);
@@ -130,7 +148,12 @@ int create_threads(shared_data_t* shared_data) {
   return error;
 }
 
-// procedure greet:
+/**
+ * @brief subrutina llamada por los hilos, indica:
+ * imprime numero de hilos y el total de hilos por 
+ * en orden por medio de semaforos
+ * @param data
+*/
 void* greet(void* data) {
   assert(data);
   private_data_t* private_data = (private_data_t*) data;
@@ -138,6 +161,7 @@ void* greet(void* data) {
 
   // Wait until it is my turn
   // wait(can_greet[thread_number])
+  // Esperar al semforo de X hilo
   int error = sem_wait(&shared_data->can_greet[private_data->thread_number]);
   if (error) {
     fprintf(stderr, "error: could not wait for semaphore\n");
@@ -149,9 +173,11 @@ void* greet(void* data) {
 
   // Allow subsequent thread to do the task
   // signal(can_greet[(thread_number + 1) mod thread_count])
+  // Indicamos cual es el siguiente hilo
   const uint64_t next_thread = (private_data->thread_number + 1)
     % shared_data->thread_count;
 
+  // Incremente el semaforo al siguiente hilo
   error = sem_post(&shared_data->can_greet[next_thread]);
   if (error) {
     fprintf(stderr, "error: could not increment semaphore\n");
