0c7ceece03819fc5bab8f71a75fed40df09faaaa
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-04-03 22:55:19 -0600

    se cambia estructura de datos, se agrega un inicializador

--- tareas/tarea01/src/datos.c
+++ tareas/tarea01/src/datos.c
@@ -1,11 +1,34 @@
+// Copyright 2023 <Dario Murillo Chaverri C15406>
 #include <datos.h>
+#include <assert.h>
+#include <errno.h>
+
+
+datos_t* datos_create(void) {
+    datos_t* datos = (datos_t*)
+    malloc(1 * sizeof(datos_t));
+    return datos;
+}
 
 void datos_innit(datos_t* datos) {
+    assert(datos);
     arreglo_innit(&datos->alfabeto);
+    arreglo_innit(&datos->zips);
+    zip_stat_init(&datos->sb);
     datos->limite = 0;
-
 }
 
 void datos_destroy(datos_t* datos) {
+    assert(datos);
+    arreglo_destroy(&datos->alfabeto);
+    arreglo_destroy(&datos->zips);
     free(datos);
+}
+
+
+int contra(datos_t* datos) {
+    int error = EXIT_SUCCESS;
+    (void)datos;    
+
+    return error;
 }
--- tareas/tarea01/src/datos.h
+++ tareas/tarea01/src/datos.h
@@ -1,19 +1,24 @@
+// Copyright 2023 <Dario Murillo Chaverri C15406>
 #ifndef DATOS_H
 #define DATOS_H
 
 #include <arr_dinamico.h>
+#include <inttypes.h>
+#include <zip.h>
 
 typedef struct datos {
     arr_dinamico_t alfabeto;
+    arr_dinamico_t zips;
     char limite;
-    char archivo1[21];
-    char archivo2[21];
-    char archivo3[21];
-}datos_t;
+    struct zip* pointer;
+    struct zip_stat sb;
+    struct zip_error_t* ze;
+} datos_t;
 
+datos_t* datos_create(void);
 void datos_innit(datos_t* datos);
 void datos_destroy(datos_t* datos);
+int contra(datos_t* datos);
 
 
-
-#endif 
+#endif
