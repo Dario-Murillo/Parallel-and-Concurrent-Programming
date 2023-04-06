e282e91ecaad90019ff4b1e3477b903538479bd6
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-04-05 19:49:28 -0600

    se corrige la carga de datos hacia al struct

--- tareas/tarea01/src/datos.c
+++ tareas/tarea01/src/datos.c
@@ -29,78 +29,75 @@ void datos_destroy(datos_t* datos) {
 int datos_analisis(datos_t* datos, FILE* input) {
     assert(datos);
     int error = EXIT_SUCCESS;
-    char value;
     char str[120];
     int linea = 0;
-    while (fgets(str, 120, input) != NULL) {
-         if (linea == 0) {
+    while (fgets(str, 120, input)) {
+        if (linea == 0) {
+            str[strcspn(str, "\n")] = 0;
             arreglo_agregar(&datos->alfabeto, str);
-        } else if (linea == 1) {
-            fscanf(input, "%c", &value);
-            datos->limite = value;
+        } else if (linea == 1 && linea < 2) {
+            str[strcspn(str, "\n")] = 0;
+            sscanf(str, "%"  SCNi64 , &datos->limite);
         } else if (linea >= 3) {
+            str[strcspn(str, "\n")] = 0;
             arreglo_agregar(&datos->zips, str);
         }
         linea++;
-    }    
-    puts(datos->alfabeto.array[0]);
+    } 
     return error;
 }
 
 
-void generate_passw(datos_t* datos) {
+void datos_generate_passw(datos_t* datos) {
     uint64_t my_limite = datos->limite;
-    char pass[my_limite+1];
-    for (size_t size = 1; size < my_limite; size++) {
-        memset(pass, 0, sizeof(pass));
-        for (size_t i = 0; i < size; i++) {
-
-        }
-        while(1) {
-            printf("%s\n", pass);
-        }
-    }
+    
 }
+
 
 /**
  * 
  * 
  * https://gist.github.com/mobius/1759816
 */
-int abrir_archivo(datos_t* datos) {
+int datos_abrir_archivo(datos_t* datos) {
     int error = EXIT_SUCCESS;
-    char* archive = "tests/zip_05/f02.zip";
-    const char* password = "75";
-    char buf[100];
+    size_t i = 0;
+    while (i <  datos->zips.total) {
+        char* archive = datos->zips.array[i];
+        const char* password = "75";
+        char buf[100];
+        
+        if ((datos->za = zip_open(archive, 0, &error)) != NULL) {
+            printf("Hola");
+        } else {
+            zip_error_to_str(buf, sizeof(buf), error, errno);
+            fprintf(stderr, "Can't open zip archive `%s': %s\n", 
+                archive, buf);
+            return error;
+        }
     
-    if ((datos->za = zip_open(datos->alfabeto.array[0], 0, &error)) != NULL) {
-        printf("Hola");
-    } else {
-        zip_error_to_str(buf, sizeof(buf), error, errno);
-        fprintf(stderr, "Can't open zip archive `%s': %s\n", 
-            archive, buf);
-        return error;
-    }
- 
-    for (zip_int64_t  i = 0; i < zip_get_num_entries(datos->za, 0); i++) {
-        if (zip_stat_index(datos->za, i, 0, &datos->sb) == 0) {
-            datos->zf = 
-            zip_fopen_index_encrypted(datos->za, i, 0, password);
-                if (datos->zf) {
-                    zip_fread(datos->zf, buf, 100);
-                    if (buf[0] == 'C') {
-                        printf("Correct");
-                    } else {
-                        //falso positivo
+        for (zip_int64_t  i = 0; i < zip_get_num_entries(datos->za, 0); i++) {
+            if (zip_stat_index(datos->za, i, 0, &datos->sb) == 0) {
+                datos->zf = 
+                zip_fopen_index_encrypted(datos->za, i, 0, password);
+                    if (datos->zf) {
+                        zip_fread(datos->zf, buf, 100);
+                        if (buf[0] == 'C') {
+                            printf("Correct");
+                            i++; // si encontro la contra pase al siguiente archivo
+                            // agregar a un arreglo el archivo y la contra 
+                        } else {
+                            //falso positivo
+                        }
                     }
                 }
+                zip_fclose(datos->zf);
             }
-            zip_fclose(datos->zf);
-        }
 
-    if (zip_close(datos->za) == -1) {
-        fprintf(stderr, "Can't close zip archive `%s'/n",archive);
-        return 1;
+        if (zip_close(datos->za) == -1) {
+            fprintf(stderr, "Can't close zip archive `%s'/n",archive);
+            return 1;
+        }
     }
     return error;
 }
--- tareas/tarea01/src/datos.h
+++ tareas/tarea01/src/datos.h
@@ -9,7 +9,7 @@
 typedef struct datos {
     arr_dinamico_t alfabeto;
     arr_dinamico_t zips;
-    uint64_t limite;
+    int64_t limite;
     struct zip* za;
     struct zip_file *zf;
     struct zip_stat sb;
@@ -17,9 +17,9 @@ typedef struct datos {
 
 datos_t* datos_create(void);
 void datos_innit(datos_t* datos);
-void datos_destroy(datos_t* datos);
 int datos_analisis(datos_t* datos, FILE* input);
-int abrir_archivo(datos_t* datos);
-void generate_passw(datos_t* datos);
+int datos_abrir_archivo(datos_t* datos);
+void datos_generate_passw(datos_t* datos);
+void datos_destroy(datos_t* datos);
 
 #endif
