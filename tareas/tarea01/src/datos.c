cc9bb6254bc90ad311ab268928b1ffec2f396b1f
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-04-06 22:45:50 -0600

    se agrega posible solucion al generador de contrasenas y se corrige problema de inicializacion

--- tareas/tarea01/src/datos.c
+++ tareas/tarea01/src/datos.c
@@ -5,48 +5,35 @@
 #include <string.h>
 #include <unistd.h>
 
-/**
- * @brief reserva memoria para crear una variable de tipo datos_t
- * @param void
- * @return variable de tipo datos_t
-*/
+
 datos_t* datos_create(void) {
     datos_t* datos = (datos_t*)
     malloc(1 * sizeof(datos_t));
     return datos;
 }
 
-/**
- * @brief inicia los miembros del struct datos
- * @param datos puntero de una variable tipo datos_t
- * @return void
-*/
+
 void datos_innit(datos_t* datos) {
     assert(datos);
     arreglo_innit(&datos->alfabeto);
     arreglo_innit(&datos->zips);
+    arreglo_innit(&datos->contrasenas);
     zip_stat_init(&datos->sb);
+    datos->za = NULL;
+    datos->zf = NULL;
     datos->limite = 0;
 }
 
-/**
- * @brief libera la memoria usada
- * @see datos_innit
-*/
+
 void datos_destroy(datos_t* datos) {
     assert(datos);
     arreglo_destroy(&datos->alfabeto);
     arreglo_destroy(&datos->zips);
+    arreglo_destroy(&datos->contrasenas);
     free(datos);
 }
 
-/**
- * @brief lee el archivo introducido por la entrada estandar
- * y carga los valores en las variables respectivas
- * @param datos puntero de una variable tipo datos_t
- * @param input archivo introducido de la entrada estandar
- * @return codigo de error 
-*/
+
 int datos_analisis(datos_t* datos, FILE* input) {
     assert(datos);
     int error = EXIT_SUCCESS;
@@ -70,71 +57,58 @@ int datos_analisis(datos_t* datos, FILE* input) {
 }
 
 
-// void datos_generate_passw(datos_t* datos, char prefix) {
-//     uint64_t my_limite = datos->limite;
-//     char prefix = "";
-//     printf("%s\n", prefix);
-    
-//     for (int i = 0; i < strlen(datos->alfabeto.array[0]); i++) {
-//         char new_prefix[strlen(prefix) + 1];
-//         strcpy(new_prefix, prefix);
-//         new_prefix[strlen(prefix)] = datos->alfabeto.array[0][i];
-//         new_prefix[strlen(prefix) + 1] = '\0';
-//         datos_generate_passw(datos, new_prefix);
-//      }
-// }
+void datos_generate_passw(datos_t* datos, char* prefix) {
+
+    if (strlen(prefix) > 0 && strlen(prefix) <= (size_t)datos->limite) {
+        printf("%s\n", prefix);
+    }
+
+    if (strlen(prefix) < (size_t)datos->limite) {
+        for (size_t i = 0; i < strlen(datos->alfabeto.array[0]); i++) {
+            char new_prefix[strlen(prefix) + 1];
+            strcpy(new_prefix, prefix);
+            new_prefix[strlen(prefix)] = datos->alfabeto.array[0][i];
+            new_prefix[strlen(prefix) + 1] = '\0';
+            datos_generate_passw(datos, new_prefix);
+        }
+    }
+}
 
 
-
-/**
- * @brief codigo encargado de abrir un archivo zip encriptado
- * @param datos puntero de una variable tipo datos_t
- * @return codigo de error
- * @details: lineas de codigo tomadas directamente
- * o como referencia de //gist.github.com/mobius/1759816
-*/
 int datos_abrir_archivo(datos_t* datos) {
     int error = EXIT_SUCCESS;
     size_t i = 0;
-    while (i <  datos->zips.total) {
-        char* archive = datos->zips.array[i];
-        const char* password = "75";
-        char buf[100];
 
-        if ((datos->za = zip_open(archive, 0, &error)) != NULL) {
-            printf("Hola");
-        } else {
-            zip_error_to_str(buf, sizeof(buf), error, errno);
-            fprintf(stderr, "Can't open zip archive `%s': %s\n",
-                archive, buf);
-            return error;
-        }
+    const char* archive = datos->zips.array[i];
+    const char* password = "75";
+    char buf[100];
 
-        for (zip_int64_t  i = 0; i < zip_get_num_entries(datos->za, 0); i++) {
-            if (zip_stat_index(datos->za, i, 0, &datos->sb) == 0) {
-                datos->zf =
-                zip_fopen_index_encrypted(datos->za, i, 0, password);
-                    if (datos->zf) {
-                        zip_fread(datos->zf, buf, 100);
-                        /// verificar que no sea un falso positivo
-                        /// al leer el contendio del archivo
-                        if (buf[0] == 'C') {
-                            printf("Correct");
-                            i++;
-                            /// si encontro la contra pase al siguiente archivo
-                            /// agregar a un arreglo el archivo y la contra
-                        } else {
-                        /// falso positivo
-                        }
+    if ((datos->za = zip_open(archive, 0, &error)) != NULL) {
+    } else {
+        zip_error_to_str(buf, sizeof(buf), error, errno);
+        fprintf(stderr, "Can't open zip archive `%s': %s\n",
+        archive, buf);
+        return error;
+    }
+
+    for (zip_int64_t  i = 0; i < zip_get_num_entries(datos->za, 0); i++) {
+        if (zip_stat_index(datos->za, i, 0, &datos->sb) == 0) {
+            datos->zf = zip_fopen_index_encrypted(datos->za, i, 0, password);
+                if (datos->zf) {
+                    zip_fread(datos->zf, buf, 100);
+                    /// verificar que no sea un falso positivo
+                    /// al leer el contendio del archivo
+                    if (buf[0] == 'C') {
+                        arreglo_agregar(&datos->contrasenas, password);               
+                    } else {
                     }
                 }
-                zip_fclose(datos->zf);
-            }
-
-        if (zip_close(datos->za) == -1) {
-            fprintf(stderr, "Can't close zip archive `%s'/n", archive);
-            return 1;
-        }
+            }   
+        zip_fclose(datos->zf);
+    }
+    if (zip_close(datos->za) == -1) {
+        fprintf(stderr, "Can't close zip archive `%s'/n", archive);
+        return 1;
     }
     return error;
 }
