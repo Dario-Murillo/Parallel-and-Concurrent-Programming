c0cf0073ed94431b7d95f82d8958175db02f8a52
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-04-20 22:05:26 -0600

    cambios en el productor

--- ejercicios/pthreads/prod_cons_unbound_produce2/src/producer.c
+++ ejercicios/pthreads/prod_cons_unbound_produce2/src/producer.c
@@ -13,21 +13,23 @@ void* produce(void* data) {
   while (true) {
 
     size_t my_unit = 0;
-    if (sem_trywait (&simulation->can_produce)) {
-        my_unit = ++simulation->next_unit;
+    if (sem_trywait(&simulation->can_produce)) {
+      
+      sem_wait(&simulation->can_acces_unit);
+      my_unit = ++simulation->next_unit;
+      sem_post(&simulation->can_acces_unit);
+      
+
+      usleep(1000 * random_between(simulation->producer_min_delay
+        , simulation->producer_max_delay));
+      queue_enqueue(&simulation->queue, my_unit);
+      printf("Produced %zu\n", my_unit);
+
+      // signal(can_consume)
+      sem_post(&simulation->can_consume);
+      sem_post(&simulation->can_produce);
     } else {
-      break;
-    }
 
-    usleep(1000 * random_between(simulation->producer_min_delay
-      , simulation->producer_max_delay));
-    queue_enqueue(&simulation->queue, my_unit);
-    printf("Produced %zu\n", my_unit);
-
-    // signal(can_consume)
-    sem_post(&simulation->can_consume);
-    if (simulation->next_unit < simulation->unit_count) {
-        sem_post(&simulation->can_produce);
     }
   }
 
