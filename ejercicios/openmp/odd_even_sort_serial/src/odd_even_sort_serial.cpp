b788f96f1034d8ce9fcca18515db3aa952f8bc3b
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-05-12 10:32:02 -0600

    se corrige el ordenamiento

--- ejercicios/openmp/odd_even_sort_serial/src/odd_even_sort_serial.cpp
+++ ejercicios/openmp/odd_even_sort_serial/src/odd_even_sort_serial.cpp
@@ -1,44 +1,52 @@
 // Copyright Dario Murillo C15406
 #include <iostream>
+#include <random>
 
-void serial_odd_even_sort(int n, double* array);
+void serial_odd_even_sort(double* array, int n);
+void swap(double* a, double* b);
 
 int main(int argc, char *argv[]) {
 
-  int size = 0;
+  int size = 5;
   if (argc >= 2) {
     size = atoi(argv[1]);
   }
   double* array = new double[size]; 
 
   for (int i = 0; i < size; i++) {
-    array[i] = rand() % 10;
+    std::random_device rseed;
+    std::mt19937 rng(rseed());
+    std::uniform_int_distribution<int> dist(0, 10);
+    array[i] = dist(rng);
   }
 
-  serial_odd_even_sort(size, array);
+  serial_odd_even_sort(array, size);
 
-  for (int i = 0; i < size; i++) {
-   std::cout << array[i] << std::endl;
-  }
 
   delete [] array;
   return 0;
 }
 
-void serial_odd_even_sort(int n, double* array) {
+void swap(double* a, double* b) {
+  double temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void serial_odd_even_sort(double* array, int n) {
   for (int phase = 0; phase < n; ++phase) {
-  if (phase % 2 == 0) {
-    for (int i = 1; i < n; i += 2) {
-      if (array[i - 1] > array[i]) {
-        std::swap(&array[i - 1], &array[i]);
+    if (phase % 2 == 0) {
+      for (int i = 1; i < n; i += 2) {
+        if (array[i - 1] > array[i]) {
+          swap(&array[i-1], &array[i]);
+        }
       }
-    }
-  } else {
-    for (int i = 1; i < n - 1; i += 2) {
-      if (array[i] > array[i + 1]) {
-          std::swap(&array[i - 1], &array[i]);
+    } else {
+      for (int i = 1; i < n - 1; i += 2) {
+        if (array[i] > array[i + 1]) {
+            swap(&array[i], &array[i+1]);
+          }
         }
       }
     }
-  }
 }
