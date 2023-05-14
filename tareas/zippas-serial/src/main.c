3674af35ed8da0a1b6eb7683104f9443b54f5980
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-05-14 17:18:10 -0600

    cambios en los llamados del main

--- tareas/zippas-serial/src/main.c
+++ tareas/zippas-serial/src/main.c
@@ -1,20 +1,18 @@
 // Copyright 2023 <Dario Murillo Chaverri C15406>
-#include <datos.h>
 #include <string.h>
+#include "simulacion.h"
 
-int main() {
-    FILE* input = stdin;
-    int error = EXIT_SUCCESS;
-    datos_t *datos = datos_create();
-    datos_innit(datos);
-    error = datos_analisis(datos, input);
-    if (error == EXIT_SUCCESS) {
-        datos_generate_passw(datos);
-        datos_impresion(datos);
-        datos_destroy(datos);
-    } else {
-        printf("No se pudieron leer los datos");
-        error = EXIT_FAILURE;
-    }
-    return error;
+int main(int argc, char* argv[]) {
+  FILE* input = stdin;
+  int error = EXIT_SUCCESS;
+  datos_t *datos = datos_create();
+  datos_innit(datos);
+  error = datos_run(datos, input, argc, argv);
+  if (error == EXIT_SUCCESS) {
+    datos_destroy(datos);
+  } else {
+    printf("No se pudieron leer los datos");
+    error = EXIT_FAILURE;
+  }
+  return error;
 }
