0ce8c35916958f5b045c09f289d8631a284b75d6
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-05-14 17:17:48 -0600

    se cambia estructura de archivos y se agrega creacion de hilos

---
+++ tareas/zippas-serial/src/simulacion.h
@@ -0,0 +1,40 @@
+// Copyright 2023 <Dario Murillo Chaverri C15406>
+#ifndef SIMULACION_H
+#define SIMULACION_H
+
+#include <arr_dinamico.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include "common.h"
+
+
+typedef struct datos datos_t;
+/**
+ * @brief reserva memoria para crear una variable de tipo datos_t
+ * @return variable de tipo datos_t
+*/
+datos_t* datos_create();
+
+/**
+ * @brief inicia los miembros del struct datos
+ * @param datos puntero de una variable tipo datos_t
+ * @return void
+*/
+void datos_innit(datos_t* datos);
+
+/**
+ * @brief controlador principal de la simulacion
+ * 
+*/
+int datos_run(datos_t* datos, FILE* input, int argc, char* argv[]); 
+
+
+/**
+ * @brief libera la memoria usada
+ * @see datos_innit
+*/
+void datos_destroy(datos_t* datos);
+
+
+
+#endif
