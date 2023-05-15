86cec85717b6ae74ca5cff2b21fb4996f91746dc
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-05-15 15:59:16 -0600

    correcion de errores del lint

--- ejercicios/openmp/odd_even_sort_serial/src/odd_even_sort_serial.cpp
+++ ejercicios/openmp/odd_even_sort_serial/src/odd_even_sort_serial.cpp
@@ -6,22 +6,23 @@ void serial_odd_even_sort(double* array, int n);
 void swap(double* a, double* b);
 
 int main(int argc, char *argv[]) {
-
   int size = 5;
   if (argc >= 2) {
     size = atoi(argv[1]);
   }
-  double* array = new double[size]; 
+  double* array = new double[size];
 
   for (int i = 0; i < size; i++) {
     std::random_device rseed;
     std::mt19937 rng(rseed());
-    std::uniform_int_distribution<int> dist(0, 10);
+    std::uniform_int_distribution<int> dist(0, 20);
     array[i] = dist(rng);
   }
 
   serial_odd_even_sort(array, size);
-
+  for (int i = 0; i < size; i++) {
+    std::cout << array[i] << std::endl;
+  }
 
   delete [] array;
   return 0;
