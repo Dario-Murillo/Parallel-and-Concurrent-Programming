2fda9685cac7f74c99435277f1fdbe895587e313
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-05-25 18:16:31 -0600

    se agrega area critica

--- ejercicios/openmp/odd_even_sort_parallel_for/src/odd_even_sort_parallel_for.cpp
+++ ejercicios/openmp/odd_even_sort_parallel_for/src/odd_even_sort_parallel_for.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < size; i++) {
     std::random_device rseed;
     std::mt19937 rng(rseed());
-    std::uniform_int_distribution<int> dist(0, 20);
+    std::uniform_int_distribution<int> dist(0, 100);
     array[i] = dist(rng);
   }
 
@@ -47,18 +47,20 @@ void parallel_odd_even_sort(double* array, int n, int thread_count) {
   for (int phase = 0; phase < n; ++phase) {
     if (phase % 2 == 0) {
       #pragma omp parallel for num_threads(thread_count) \
-        default(none) shared(array, n, std::cout)
+        default(none) shared(array, n)
       for (int i = 1; i < n; i += 2) {
+        #pragma omp critical
         if (array[i - 1] > array[i]) {
           swap(&array[i-1], &array[i]);
         }
       }
     } else {
       #pragma omp parallel for num_threads(thread_count) \
-        default(none) shared(array, n, std::cout)
+        default(none) shared(array, n)
       for (int i = 1; i < n - 1; i += 2) {
+        #pragma omp critical
         if (array[i] > array[i + 1]) {
-            swap(&array[i], &array[i+1]);
+          swap(&array[i], &array[i+1]);
         }
       }
     }
