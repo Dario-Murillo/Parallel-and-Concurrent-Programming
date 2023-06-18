af8b309e7eed3d408de864d95a0bbd5c012414f7
Dario Murillo <dario.murillo@ucr.ac.cr>
2023-06-17 22:39:07 -0600

    se corrige error al enviar los numeros a los procesos

--- ejercicios/mpi/hybrid_distr_stdin/src/hybrid_distr_stdin.cpp
+++ ejercicios/mpi/hybrid_distr_stdin/src/hybrid_distr_stdin.cpp
@@ -45,7 +45,7 @@ int main(int argc, char* argv[]) {
 
     int overall_start = 0;
     int overall_finish = 0;
-    unsigned int thread_number = 3;
+    unsigned int thread_number = std::thread::hardware_concurrency();
 
     if (argc == 3) {
       // si se ingresaron argumentos en la linea de comandos
@@ -58,7 +58,7 @@ int main(int argc, char* argv[]) {
         std::cin >> overall_start;
         std::cout << "Valor final " << std::endl;
         std::cin >> overall_finish;
-        for (int target = 0; target < process_count; target++) {
+        for (int target = 1; target < process_count; target++) {
           if (MPI_Send(&overall_start, 1, MPI_INT, target
           , /*tag*/ 0, MPI_COMM_WORLD) != MPI_SUCCESS) {
             throw std::runtime_error("could not send message");
